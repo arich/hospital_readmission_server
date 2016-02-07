d66db9947579db65aae23bd67230100b59ae5220
Andrew Rich <andrew.joseph.rich@gmail.com>
2016-02-06 22:10:45 -0600

    Added migration to populate hospitals table (not working yet)
    - Added a dir to hold the flat file csv
    - Added a migration to populate the hospitals table with that csv, using postgres'
    COPY function
    - Learned about elixir's abspath method, that was pretty useful
    - Currently, the copy is erroring out on a column that's an integer that has "Not Available" instead.
    - I think this is a job for pandas. Let's clean the file and perhaps properly capitalize the names
    and go from there.

---
+++ priv/repo/migrations/20160207012812_copy_hospital_csv.exs
@@ -0,0 +1,58 @@
+defmodule HospitalReadmissionServer.Repo.Migrations.CopyHospitalCsv do
+  use Ecto.Migration
+
+  def up do
+    abs_path = Path.absname('priv/repo/data_files/Hospital_Readmissions_Reduction_Program.csv')
+    IO.puts(abs_path)
+
+    # Remove not null restriction on inserted_at and updated_at
+    execute """
+      ALTER TABLE hospitals
+      ALTER COLUMN inserted_at
+      DROP NOT NULL,
+      ALTER COLUMN updated_at
+      DROP NOT NULL;
+    """
+
+    execute """
+      COPY hospitals(
+        name,
+        provider_number,
+        state,
+        measure_name,
+        number_of_discharges,
+        footnote,
+        excess_readmission_ratio,
+        predicted_readmission_ratio,
+        expected_readmission_rate,
+        number_of_readmissions,
+        start_date,
+        end_date
+      )
+      FROM '#{abs_path}'
+      WITH CSV HEADER;
+    """
+
+    execute """
+      UPDATE hospitals
+        SET inserted_at = current_time,
+        updated_at = current_time
+      WHERE
+        inserted_at IS NULL
+        OR updated_at IS NULL;
+    """
+
+    # Readd not null restriction on inserted_at and updated_at
+    execute """
+      ALTER TABLE hospitals
+      ALTER COLUMN inserted_at
+      SET NOT NULL,
+      ALTER COLUMN updated_at
+      SET NOT NULL;
+    """
+  end
+
+  def down do
+
+  end
+end
