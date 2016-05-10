946b5861bc8926f9ac5dbf9cc9417f0835cfc50e
Elijah Bernstein-Cooper <e.bernsteincooper@gmail.com>
2016-05-09 21:05:41 -0500

    lengthened comment with nonsense

--- scripts/census_table.sql
+++ scripts/census_table.sql
@@ -2,7 +2,7 @@
 
 ALTER TABLE states ADD CONSTRAINT abbrev_key UNIQUE (abbrev);
 
-# create column for state ID
+# create column for state ID changed comment
 ALTER TABLE hospitals ADD COLUMN state_id integer;
 
 # update hospitals
